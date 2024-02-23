7ea062154d7d8657cca11a1d25c467769285f283
Franco Brown <fbrownp.ic@gmail.com>
2024-02-23 11:16:44 -0300

    New commit

--- src/Churn_analysis/components/model_evaluation.py
+++ src/Churn_analysis/components/model_evaluation.py
@@ -45,7 +45,8 @@ class ModelEvaluation():
 
         mlflow.set_registry_uri(self.config.mlflow_uri)
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
-        mlflow.set_experiment(timestamp)
+        # mlflow.set_experiment(timestamp)
+        mlflow.set_tag("mlflow.runName", f"{timestamp}")
 
 
 
